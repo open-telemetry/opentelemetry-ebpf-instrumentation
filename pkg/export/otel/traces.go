321f35f105952d8def264957deec9924ac29cf81
Mario Macias <mario.macias@grafana.com>
2025-06-18 12:53:22 +0200

    Generate Traces: allow overriding resource attributes

--- pkg/export/otel/traces.go
+++ pkg/export/otel/traces.go
@@ -499,13 +499,26 @@ func traceAppResourceAttrs(cache *expirable2.LRU[svc.UID, []attribute.KeyValue],
 	return attrs
 }
 
-func generateTracesWithAttributes(cache *expirable2.LRU[svc.UID, []attribute.KeyValue], svc *svc.Attrs, envResourceAttrs []attribute.KeyValue, hostID string, spans []TraceSpanAndAttributes) ptrace.Traces {
+func generateTracesWithAttributes(
+	cache *expirable2.LRU[svc.UID, []attribute.KeyValue],
+	svc *svc.Attrs,
+	envResourceAttrs []attribute.KeyValue,
+	hostID string,
+	spans []TraceSpanAndAttributes,
+	opts ...ResourceOpt,
+) ptrace.Traces {
+	opt := resourceOptions{}
+	for _, o := range opts {
+		o(&opt)
+	}
+
 	traces := ptrace.NewTraces()
 	rs := traces.ResourceSpans().AppendEmpty()
 	resourceAttrs := traceAppResourceAttrs(cache, hostID, svc)
 	resourceAttrs = append(resourceAttrs, envResourceAttrs...)
 	resourceAttrsMap := attrsToMap(resourceAttrs)
 	resourceAttrsMap.PutStr(string(semconv.OTelLibraryNameKey), reporterName)
+	addAttrsToMap(opt.overrideAttrs, resourceAttrsMap)
 	resourceAttrsMap.MoveTo(rs.Resource().Attributes())
 
 	for _, spanWithAttributes := range spans {
@@ -557,8 +570,15 @@ func generateTracesWithAttributes(cache *expirable2.LRU[svc.UID, []attribute.Key
 }
 
 // GenerateTraces creates a ptrace.Traces from a request.Span
-func GenerateTraces(cache *expirable2.LRU[svc.UID, []attribute.KeyValue], svc *svc.Attrs, envResourceAttrs []attribute.KeyValue, hostID string, spans []TraceSpanAndAttributes) ptrace.Traces {
-	return generateTracesWithAttributes(cache, svc, envResourceAttrs, hostID, spans)
+func GenerateTraces(
+	cache *expirable2.LRU[svc.UID, []attribute.KeyValue],
+	svc *svc.Attrs,
+	envResourceAttrs []attribute.KeyValue,
+	hostID string,
+	spans []TraceSpanAndAttributes,
+	opts ...ResourceOpt,
+) ptrace.Traces {
+	return generateTracesWithAttributes(cache, svc, envResourceAttrs, hostID, spans, opts...)
 }
 
 // createSubSpans creates the internal spans for a request.Span
@@ -591,20 +611,24 @@ func createSubSpans(span *request.Span, parentSpanID pcommon.SpanID, traceID pco
 // attrsToMap converts a slice of attribute.KeyValue to a pcommon.Map
 func attrsToMap(attrs []attribute.KeyValue) pcommon.Map {
 	m := pcommon.NewMap()
-	m.EnsureCapacity(len(attrs))
+	addAttrsToMap(attrs, m)
+	return m
+}
+
+func addAttrsToMap(attrs []attribute.KeyValue, dst pcommon.Map) {
+	dst.EnsureCapacity(dst.Len() + len(attrs))
 	for _, attr := range attrs {
 		switch v := attr.Value.AsInterface().(type) {
 		case string:
-			m.PutStr(string(attr.Key), v)
+			dst.PutStr(string(attr.Key), v)
 		case int64:
-			m.PutInt(string(attr.Key), v)
+			dst.PutInt(string(attr.Key), v)
 		case float64:
-			m.PutDouble(string(attr.Key), v)
+			dst.PutDouble(string(attr.Key), v)
 		case bool:
-			m.PutBool(string(attr.Key), v)
+			dst.PutBool(string(attr.Key), v)
 		}
 	}
-	return m
 }
 
 // codeToStatusCode converts a codes.Code to a ptrace.StatusCode
