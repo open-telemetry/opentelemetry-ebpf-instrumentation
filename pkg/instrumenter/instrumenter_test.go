089f83e7ceb68ee09af6c0fdbb36763c6261d69c
Marc Tuduri <marctc@protonmail.com>
2025-06-20 16:12:54 +0200

    fix stuff

--- pkg/instrumenter/instrumenter_test.go
+++ pkg/instrumenter/instrumenter_test.go
@@ -3,17 +3,15 @@ package instrumenter
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/transform"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/beyla"
 )
 
 func TestServiceNameTemplate(t *testing.T) {
+
 	cfg := &beyla.Config{
 		Attributes: beyla.Attributes{
 			Kubernetes: transform.KubernetesDecorator{
@@ -23,20 +21,18 @@ func TestServiceNameTemplate(t *testing.T) {
 	}
 
 	temp, err := buildServiceNameTemplate(cfg)
-	assert.Nil(t, temp)
-	if assert.Error(t, err) {
-		assert.Equal(t, `unable to parse service name template: template: serviceNameTemplate:1: function "asdf" not defined`, err.Error())
-	}
+	require.Nil(t, temp)
+	require.Error(t, err)
+	require.Equal(t, `unable to parse service name template: template: serviceNameTemplate:1: function "asdf" not defined`, err.Error())
 
 	cfg.Attributes.Kubernetes.ServiceNameTemplate = `{{- if eq .Meta.Pod nil }}{{.Meta.Name}}{{ else }}{{- .Meta.Namespace }}/{{ index .Meta.Labels "app.kubernetes.io/name" }}/{{ index .Meta.Labels "app.kubernetes.io/component" -}}{{ if .ContainerName }}/{{ .ContainerName -}}{{ end -}}{{ end -}}`
 	temp, err = buildServiceNameTemplate(cfg)
 
 	require.NoError(t, err)
-	assert.NotNil(t, temp)
+	require.NotNil(t, temp)
 
 	cfg.Attributes.Kubernetes.ServiceNameTemplate = ""
 	temp, err = buildServiceNameTemplate(cfg)
 	require.NoError(t, err)
-	assert.Nil(t, temp)
-	assert.NoError(t, err)
+	require.Nil(t, temp)
 }
