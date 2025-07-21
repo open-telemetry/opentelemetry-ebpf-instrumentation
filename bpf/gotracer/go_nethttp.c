6e2d96f476d2af1cdd641710ece07efdb2b7ca7d
Rafael Roquetto <rafael.roquetto@grafana.com>
2025-07-21 13:20:18 -0600

    Fix spelling mistake

--- bpf/gotracer/go_nethttp.c
+++ bpf/gotracer/go_nethttp.c
@@ -1284,7 +1284,7 @@ int obi_uprobe_jsonrpcReadRequestHeaderReturns(struct pt_regs *ctx) {
     off_table_t *ot = get_offsets_table();
     const u64 rpc_request_addr = invocation->rpc_request_addr;
     bpf_dbg_printk("rpc_request_addr %llx", rpc_request_addr);
-    const u64 method_len = peak_go_str_len(
+    const u64 method_len = peek_go_str_len(
         "JSON-RPC method",
         (void *)rpc_request_addr,
         go_offset_of(ot, (go_offset){.v = _jsonrpc_request_header_service_method_pos}));
