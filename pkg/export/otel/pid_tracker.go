ad6f445dfe8bf43cfffba06c99ee817aefdb2687
Nikola Grcevski <grcevski@gmail.com>
2025-09-17 17:04:44 -0400

    handle duplicates for target info - prometheus

--- pkg/export/otel/pid_tracker.go
+++ pkg/export/otel/pid_tracker.go
@@ -64,6 +64,32 @@ func (p *PidServiceTracker) RemovePID(pid int32) (bool, svc.UID) {
 	return false, svc.UID{}
 }
 
+func (p *PidServiceTracker) TracksPID(pid int32) (svc.UID, bool) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	u, ok := p.pidToService[pid]
+
+	return u, ok
+}
+
+func (p *PidServiceTracker) UpdateUID(staleUID, newUID svc.UID) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if staleUID.Equals(&newUID) {
+		return
+	}
+
+	if pids, ok := p.servicePIDs[staleUID]; ok {
+		for pid := range pids {
+			p.pidToService[pid] = newUID
+		}
+		p.servicePIDs[newUID] = pids
+		delete(p.servicePIDs, staleUID)
+	}
+}
+
 func (p *PidServiceTracker) Count() int {
 	p.lock.Lock()
 	defer p.lock.Unlock()
