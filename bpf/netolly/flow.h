e48ff56719c08b043e09a2c8912cac09ca63aa93
Rafael Roquetto <rafael.roquetto@grafana.com>
2025-09-25 10:02:55 -0600

    Adjust variable sizes

--- bpf/netolly/flow.h
+++ bpf/netolly/flow.h
@@ -54,10 +54,12 @@ typedef struct flow_metrics_t {
     u64 end_mono_time_ns;
 
     // direction of the flow EGRESS / INGRESS
-    u64 iface_direction;
+    u8 iface_direction;
 
     // who initiated of the connection: INITIATOR_SRC or INITIATOR_DST
-    u64 initiator;
+    u8 initiator;
+
+    u8 _pad[6];
 } flow_metrics;
 
 // Attributes that uniquely identify a flow
