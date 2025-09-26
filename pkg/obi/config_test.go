9d74cc1dd8ec55d33bbe5059dac2fa3b59446620
Nikola Grcevski <grcevski@gmail.com>
2025-09-26 10:10:50 -0400

    Fix traces export too frequent

--- pkg/obi/config_test.go
+++ pkg/obi/config_test.go
@@ -165,6 +165,7 @@ discovery:
 			CommonEndpoint:    "localhost:3131",
 			TracesEndpoint:    "localhost:3232",
 			MaxQueueSize:      4096,
+			BatchTimeout:      15 * time.Second,
 			ReportersCacheLen: ReporterLRUSize,
 			Instrumentations: []string{
 				instrumentations.InstrumentationALL,
