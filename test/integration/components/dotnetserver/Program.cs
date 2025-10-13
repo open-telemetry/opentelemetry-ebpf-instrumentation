f31e8170605ae2371203803e881ffff73689d118
Nikola Grcevski <grcevski@gmail.com>
2025-10-13 05:38:15 -0400

    review feedback

--- test/integration/components/dotnetserver/Program.cs
+++ test/integration/components/dotnetserver/Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 
 app.MapGet("/greeting", async (HttpClient httpClient) =>
         {
-            var response = await httpClient.GetAsync("https://opentelemetry.io/ecosystem/");
+            var response = await httpClient.GetAsync("https://opentelemetry.io/");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
             return Results.Ok(content);
