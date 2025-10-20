fbf7936787a1eefe38d732e4e887959fc9c3c6c6
Nikola Grcevski <grcevski@gmail.com>
2025-10-20 15:19:39 -0400

    test no delete

--- bpf/generictracer/k_tracer.c
+++ bpf/generictracer/k_tracer.c
@@ -286,10 +286,10 @@ cleanup:
 static __always_inline void
 tcp_send_ssl_check(u64 id, void *ssl, pid_connection_info_t *p_conn, u16 orig_dport) {
     bpf_dbg_printk("=== kprobe SSL tcp_sendmsg=%d ssl=%llx ===", id, ssl);
-    ssl_pid_connection_info_t *s_conn = bpf_map_lookup_elem(&ssl_to_conn, &ssl);
-    if (s_conn) {
-        finish_possible_delayed_tls_http_request(&s_conn->p_conn, ssl);
-    }
+    // ssl_pid_connection_info_t *s_conn = bpf_map_lookup_elem(&ssl_to_conn, &ssl);
+    // if (s_conn) {
+    //     finish_possible_delayed_tls_http_request(&s_conn->p_conn, ssl);
+    // }
     ssl_pid_connection_info_t ssl_conn = {
         .orig_dport = orig_dport,
     };
