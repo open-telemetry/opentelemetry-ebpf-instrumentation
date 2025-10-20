c0c71b8ff4a8088952761946efc4926768e9564a
Nikola Grcevski <grcevski@gmail.com>
2025-10-20 15:32:59 -0400

    fix handling of the ssl pointer

--- bpf/generictracer/k_tracer.c
+++ bpf/generictracer/k_tracer.c
@@ -350,7 +350,7 @@ int BPF_KPROBE(obi_kprobe_tcp_sendmsg, struct sock *sk, struct msghdr *msg, size
         connect_ssl_to_connection(id, &s_args.p_conn, TCP_SEND, orig_dport);
         setup_connection_to_pid_mapping(id, &s_args.p_conn, orig_dport);
 
-        void *ssl = is_ssl_connection(&s_args.p_conn);
+        u64 *ssl = is_ssl_connection(&s_args.p_conn);
         if (size > 0) {
             if (!ssl) {
                 unsigned char *buf = iovec_memory();
@@ -413,7 +413,7 @@ int BPF_KPROBE(obi_kprobe_tcp_sendmsg, struct sock *sk, struct msghdr *msg, size
             return 0;
         }
 
-        tcp_send_ssl_check(id, ssl, &s_args.p_conn, orig_dport);
+        tcp_send_ssl_check(id, (void *)(*ssl), &s_args.p_conn, orig_dport);
         bpf_map_delete_elem(&active_send_args, &id);
     }
 
@@ -450,7 +450,7 @@ int BPF_KPROBE(obi_kprobe_tcp_rate_check_app_limited, struct sock *sk) {
         setup_connection_to_pid_mapping(id, &s_args.p_conn, orig_dport);
         cp_support_established(&s_args.p_conn);
 
-        void *ssl = is_ssl_connection(&s_args.p_conn);
+        u64 *ssl = is_ssl_connection(&s_args.p_conn);
         if (!ssl) {
             msg_buffer_t *m_buf = bpf_map_lookup_elem(&msg_buffers, &e_key);
             if (m_buf) {
@@ -482,7 +482,7 @@ int BPF_KPROBE(obi_kprobe_tcp_rate_check_app_limited, struct sock *sk) {
             }
         } else {
             make_inactive_sk_buffer(&s_args.p_conn.conn);
-            tcp_send_ssl_check(id, ssl, &s_args.p_conn, orig_dport);
+            tcp_send_ssl_check(id, (void *)(*ssl), &s_args.p_conn, orig_dport);
         }
     }
 
@@ -808,7 +808,7 @@ static __always_inline int return_recvmsg(void *ctx, struct sock *in_sock, u64 i
         cp_support_established(&info);
         setup_connection_to_pid_mapping(id, &info, orig_dport);
 
-        void *ssl = is_ssl_connection(&info);
+        u64 *ssl = is_ssl_connection(&info);
 
         if (!ssl) {
 
@@ -841,7 +841,8 @@ static __always_inline int return_recvmsg(void *ctx, struct sock *in_sock, u64 i
         } else {
             // must delete the backup buffer to avoid replay
             delete_backup_sk_buff(&info.conn);
-            bpf_dbg_printk("tcp_recvmsg for an identified SSL connection, ignoring [%llx]...", ssl);
+            bpf_dbg_printk("tcp_recvmsg for an identified SSL connection, ignoring [%llx]...",
+                           *ssl);
         }
     }
 
