79b193edc49a1a3da605bfc83b7fce4749e5789e
Nikola Grcevski <grcevski@gmail.com>
2025-11-12 10:14:12 -0500

    add offsets for gin and mux

---
+++ configs/offsets/gin/inspect.go
@@ -0,0 +1,27 @@
+package gin
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log := slog.With("component", "gin.Server")
+	r := gin.Default()
+
+	r.GET("/api/users/:id/posts/:postID", func(c *gin.Context) {
+		userID := c.Param("id")
+		postID := c.Param("postID")
+		c.JSON(200, gin.H{
+			"user_id": userID,
+			"post_id": postID,
+		})
+	})
+
+	address := fmt.Sprintf(":%d", 8090)
+	log.Info("starting HTTP server", "address", address)
+	err := r.Run(address)
+	log.Error("HTTP server has unexpectedly stopped", "error", err)
+}
