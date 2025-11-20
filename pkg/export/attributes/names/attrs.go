57b446b254dcdac2ce12a59db5b69c253128672f
Nikola Grcevski <grcevski@gmail.com>
2025-11-20 12:29:20 -0500

    Allow overriding SDK name

--- pkg/export/attributes/names/attrs.go
+++ pkg/export/attributes/names/attrs.go
@@ -88,6 +88,7 @@ const (
 // as custom metrics, since at the moment they don't follow any semantic convention for them.
 // This value can be overridden when OBI is vendored as a library (e.g. from the OTEL collector)
 var VendorPrefix = "obi"
+var VendorSDKName = "opentelemetry-ebpf-instrumentation"
 
 var OBIIP = Name("obi.ip")
 
