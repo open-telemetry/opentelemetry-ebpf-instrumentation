d6aa0b31268c9379be7d9a7286bb75b849f5585c
NimrodAvni78 <nimrod@coralogix.com>
2025-12-01 17:19:11 +0200

    fix wording

--- pkg/ebpf/instrumenter.go
+++ pkg/ebpf/instrumenter.go
@@ -342,7 +342,7 @@ func (i *instrumenter) handleSockFilterErr(originalErr error, filter *ebpf.Progr
 	if err != nil {
 		return originalErr
 	}
-	return fmt.Errorf("ran out of memory, socket filter has a jitted size of %d, consider increasing the value net.core.optmem_max kernel parameter to be larger then the program size, this will not effect existing sockets but only future ones created", jittedSize)
+	return fmt.Errorf("cannot allocate memory, socket filter has a jitted size of %d, consider increasing the value net.core.optmem_max kernel parameter to be larger then the program jitted size, this will not affect existing sockets but only future ones created", jittedSize)
 }
 
 func attachSocketFilter(filter *ebpf.Program) (int, error) {
