80ea1fcd2f726cf5391808c885e8f0ab6620ee58
Marc Tudurí <marctc@protonmail.com>
2025-12-15 11:57:42 +0100

    Update bpf/gotracer/go_sql.c
    Co-authored-by: Rafael Roquetto <rafaelroquetto@users.noreply.github.com>

--- bpf/gotracer/go_sql.c
+++ bpf/gotracer/go_sql.c
@@ -48,7 +48,7 @@ static __always_inline void *get_mysql_conn_ptr(u64 driver_conn_ptr) {
     int res =
         bpf_probe_read_user(&ci_type_ptr, sizeof(ci_type_ptr), (void *)(driver_conn_ptr + ci_offset));
 
-    if (res || !ci_type_ptr) {
+    if (res != 0) {
         bpf_dbg_printk("can't read driverConn.ci type pointer");
         return NULL;
     }
