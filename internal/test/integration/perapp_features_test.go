33974d92186e8f5e87d139af48eb8339d3e3b041
Mario Macias <mario.macias@grafana.com>
2025-12-19 16:24:10 +0100

    Working OTEL integration tests

--- internal/test/integration/perapp_features_test.go
+++ internal/test/integration/perapp_features_test.go
@@ -19,35 +19,38 @@ import (
 	"go.opentelemetry.io/obi/internal/test/integration/components/docker"
 )
 
-func TestPerAppFeatures(t *testing.T) {
-	compose, err := docker.ComposeSuite("docker-compose-multiexec.yml",
-		path.Join(pathOutput, "test-suite-multiexec-perapp.log"))
+func TestPerAppFeatures_OTEL(t *testing.T) {
+	compose, err := docker.ComposeSuite("docker-compose-perapp.yml",
+		path.Join(pathOutput, "test-suite-multiexec-perapp-otel.log"))
 	require.NoError(t, err)
 
-	// we are going to setup discovery directly in the configuration file
-	compose.Env = append(compose.Env, `OTEL_EBPF_EXECUTABLE_PATH=`, `OTEL_EBPF_OPEN_PORT=`,
-		"OTEL_EBPF_CONFIG_SUFFIX", "-perapp")
 	require.NoError(t, compose.Up())
 
 	t.Run("all the services have span metrics", func(t *testing.T) {
-		checkSpanMetric(t, 3*time.Minute, "nodejs-service", 3031, "/testing-node")
-		checkSpanMetric(t, time.Minute, "rails-service", 3041, "/testing-rails")
+		checkSpanMetric(t, 3*time.Minute, "node", 3031, "/testing-node")
+		checkSpanMetric(t, time.Minute, "ruby", 3041, "/testing-rails")
+		checkSpanMetric(t, time.Minute, "pytestserver", 7773, "/testing-python")
 		checkSpanMetric(t, time.Minute, "testserver", 8080, "/testing-go")
-		checkSpanMetric(t, time.Minute, "java-service", 8086, "/testing-java")
-		checkSpanMetric(t, time.Minute, "rust-service", 8091, "/testing-rust")
+		checkSpanMetric(t, time.Minute, "jtestserver", 8086, "/testing-java")
+		checkSpanMetric(t, time.Minute, "rtestserver", 8091, "/testing-rust")
 	})
-	t.Run("node and rails have RED metrics", func(t *testing.T) {
-		t.Fail()
+	t.Run("node, rails and python have RED metrics", func(t *testing.T) {
+		hasREDMetrics(t, "node", "/testing-node")
+		hasREDMetrics(t, "ruby", "/testing-rails")
+		hasREDMetrics(t, "pytestserver", "/testing-python")
 	})
 	t.Run("rest of services don't have RED metrics", func(t *testing.T) {
-		t.Fail()
+		hasNotREDMetrics(t, "testserver")
+		hasNotREDMetrics(t, "jtestserver")
+		hasNotREDMetrics(t, "rtestserver")
 	})
 
 	require.NoError(t, compose.Close())
 }
 
+var pq = prom.Client{HostPort: prometheusHostPort}
+
 func checkSpanMetric(t *testing.T, timeout time.Duration, serviceName string, port int, path string) {
-	pq := prom.Client{HostPort: prometheusHostPort}
 	test.Eventually(t, timeout, func(t require.TestingT) {
 		// first, verify that the test service endpoint is healthy
 		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d%s", port, path), nil)
@@ -61,4 +64,16 @@ func checkSpanMetric(t *testing.T, timeout time.Duration, serviceName string, po
 	}, test.Interval(time.Second))
 }
 
-//func checkApp
+func hasREDMetrics(t *testing.T, serviceName string, path string) {
+	test.Eventually(t, time.Minute, func(t require.TestingT) {
+		results, err := pq.Query(`http_server_request_body_size_bytes_sum{service_name="` + serviceName + `",http_route="` + path + `"}`)
+		require.NoError(t, err)
+		require.NotEmpty(t, results)
+	}, test.Interval(time.Second))
+}
+
+func hasNotREDMetrics(t *testing.T, serviceName string) {
+	results, err := pq.Query(`http_server_request_body_size_bytes_sum{service_name="` + serviceName + `"}`)
+	require.NoError(t, err)
+	require.Empty(t, results)
+}
