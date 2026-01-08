3ef2c5d951594cfca84f8ff850f8c5c817356622
Mario Macias <mario.macias@grafana.com>
2026-01-08 16:08:33 +0100

    Add prometheus integration tests

--- internal/test/integration/perapp_features_test.go
+++ internal/test/integration/perapp_features_test.go
@@ -19,38 +19,47 @@ import (
 	"go.opentelemetry.io/obi/internal/test/integration/components/prom"
 )
 
-func TestPerAppFeatures_OTEL(t *testing.T) {
+func TestPerAppFeatures(t *testing.T) {
 	compose, err := docker.ComposeSuite("docker-compose-perapp.yml",
-		path.Join(pathOutput, "test-suite-multiexec-perapp-otel.log"))
+		path.Join(pathOutput, "test-suite-multiexec-perapp.log"))
 	require.NoError(t, err)
 
 	require.NoError(t, compose.Up())
 
+	t.Run("OTEL exporter", func(t *testing.T) {
+		testPerAppFeatures(t, "otel")
+	})
+	t.Run("Prometheus exporter", func(t *testing.T) {
+		testPerAppFeatures(t, "prometheus")
+	})
+
+	require.NoError(t, compose.Close())
+}
+
+func testPerAppFeatures(t *testing.T, exportedSource string) {
 	t.Run("all the services have span metrics", func(t *testing.T) {
-		checkSpanMetric(t, 3*time.Minute, "node", 3031, "/testing-node")
-		checkSpanMetric(t, time.Minute, "ruby", 3041, "/testing-rails")
-		checkSpanMetric(t, time.Minute, "pytestserver", 7773, "/testing-python")
-		checkSpanMetric(t, time.Minute, "testserver", 8080, "/testing-go")
-		checkSpanMetric(t, time.Minute, "jtestserver", 8086, "/testing-java")
-		checkSpanMetric(t, time.Minute, "rtestserver", 8091, "/testing-rust")
+		checkSpanMetric(t, 3*time.Minute, exportedSource, "node", 3031, "/testing-node")
+		checkSpanMetric(t, time.Minute, exportedSource, "ruby", 3041, "/testing-rails")
+		checkSpanMetric(t, time.Minute, exportedSource, "pytestserver", 7773, "/testing-python")
+		checkSpanMetric(t, time.Minute, exportedSource, "testserver", 8080, "/testing-go")
+		checkSpanMetric(t, time.Minute, exportedSource, "jtestserver", 8086, "/testing-java")
+		checkSpanMetric(t, time.Minute, exportedSource, "rtestserver", 8091, "/testing-rust")
 	})
 	t.Run("node, rails and python have RED metrics", func(t *testing.T) {
-		hasREDMetrics(t, "node", "/testing-node")
-		hasREDMetrics(t, "ruby", "/testing-rails")
-		hasREDMetrics(t, "pytestserver", "/testing-python")
+		hasREDMetrics(t, exportedSource, "node", "/testing-node")
+		hasREDMetrics(t, exportedSource, "ruby", "/testing-rails")
+		hasREDMetrics(t, exportedSource, "pytestserver", "/testing-python")
 	})
 	t.Run("rest of services don't have RED metrics", func(t *testing.T) {
 		hasNotREDMetrics(t, "testserver")
 		hasNotREDMetrics(t, "jtestserver")
 		hasNotREDMetrics(t, "rtestserver")
 	})
-
-	require.NoError(t, compose.Close())
 }
 
 var pq = prom.Client{HostPort: prometheusHostPort}
 
-func checkSpanMetric(t *testing.T, timeout time.Duration, serviceName string, port int, path string) {
+func checkSpanMetric(t *testing.T, timeout time.Duration, exportedSource, serviceName string, port int, path string) {
 	test.Eventually(t, timeout, func(t require.TestingT) {
 		// first, verify that the test service endpoint is healthy
 		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d%s", port, path), nil)
@@ -58,15 +67,17 @@ func checkSpanMetric(t *testing.T, timeout time.Duration, serviceName string, po
 		_, err = testHTTPClient.Do(req)
 		require.NoError(t, err)
 
-		results, err := pq.Query(`traces_spanmetrics_latency_sum{service_name="` + serviceName + `",span_name="GET ` + path + `"}`)
+		results, err := pq.Query(`traces_spanmetrics_latency_sum{exported="` + exportedSource +
+			`",service_name="` + serviceName + `",span_name="GET ` + path + `"}`)
 		require.NoError(t, err)
 		require.NotEmpty(t, results)
 	}, test.Interval(time.Second))
 }
 
-func hasREDMetrics(t *testing.T, serviceName string, path string) {
+func hasREDMetrics(t *testing.T, exportedSource, serviceName string, path string) {
 	test.Eventually(t, time.Minute, func(t require.TestingT) {
-		results, err := pq.Query(`http_server_request_body_size_bytes_sum{service_name="` + serviceName + `",http_route="` + path + `"}`)
+		results, err := pq.Query(`http_server_request_body_size_bytes_sum{exported="` + exportedSource +
+			`",service_name="` + serviceName + `",http_route="` + path + `"}`)
 		require.NoError(t, err)
 		require.NotEmpty(t, results)
 	}, test.Interval(time.Second))
