1be61af2658e3b201ff65a66e64ab6c861111d71
Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>
2026-02-11 14:55:47 +0100

    Add e2e integration test
    Signed-off-by: Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>

---
+++ internal/test/integration/components/go-app-net-metrics-client/go.mod
@@ -0,0 +1,3 @@
+module go.opentelemetry.io/obi/internal/test/integration/components/go-app-net-metrics-client
+
+go 1.25.6
---
+++ internal/test/integration/red_test_app_net_metrics.go
@@ -0,0 +1,51 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package integration // import "go.opentelemetry.io/obi/internal/test/integration"
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/obi/internal/test/integration/components/promtest"
+)
+
+func testAppNetMetricsTCPRttForGo(t *testing.T, comm, namespace string) {
+	// Eventually, Prometheus would make this query visible
+	pq := promtest.Client{HostPort: prometheusHostPort}
+	var results []promtest.Result
+	require.EventuallyWithT(t, func(ct *assert.CollectT) {
+		var err error
+		results, err = pq.Query(`obi_net_tcp_rtt_seconds_bucket{` +
+			`host_name="obi",` +
+			`service_namespace="` + namespace + `",` +
+			`service_name="` + comm + `"}`)
+		require.NoError(ct, err)
+		enoughPromResults(ct, results)
+		val := totalPromCount(ct, results)
+		assert.LessOrEqual(ct, 1, val)
+
+		results, err = pq.Query(`obi_net_tcp_rtt_seconds_count{` +
+			`host_name="obi",` +
+			`service_namespace="` + namespace + `",` +
+			`service_name="` + comm + `"}`)
+		require.NoError(ct, err)
+		enoughPromResults(ct, results)
+		val = totalPromCount(ct, results)
+		assert.LessOrEqual(ct, 1, val)
+	}, testTimeout, 100*time.Millisecond)
+}
+
+func testAppNetMetricsTCPRttGo(t *testing.T) {
+	for _, testCaseURL := range []string{
+		"http://localhost:8381",
+	} {
+		t.Run(testCaseURL, func(t *testing.T) {
+			waitForTestComponentsTCP(t, testCaseURL)
+			testAppNetMetricsTCPRttForGo(t, "testserver", "integration-test")
+		})
+	}
+}
