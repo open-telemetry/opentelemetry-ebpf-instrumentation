4fbd0cb2a87ac450f79450811129a75497f0672d
Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>
2026-02-12 08:47:03 +0100

    Add source and destination ports attributes
    Signed-off-by: Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>

--- pkg/appolly/app/request/metric_attributes.go
+++ pkg/appolly/app/request/metric_attributes.go
@@ -294,6 +294,14 @@ func Metadata(val string) attribute.KeyValue {
 	return attribute.Key(attr.GenAIMetadata).String(val)
 }
 
+func SourcePort(val int) attribute.KeyValue {
+	return attribute.Key(attr.SrcPort).Int(val)
+}
+
+func DestinationPort(val int) attribute.KeyValue {
+	return attribute.Key(attr.DstPort).Int(val)
+}
+
 // These are defined here https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__TYPES.html#group__CUDART__TYPES_1gg18fa99055ee694244a270e4d5101e95bdeec295de8a74ac2a74f98ffb6c5d7c7
 // in the enum cudaMemcpyKind
 const (
