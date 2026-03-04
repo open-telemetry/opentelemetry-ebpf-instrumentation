4e2aaa0160f7f6f176ce6d6fcd24d0ddb8ec9bb8
Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>
2026-03-04 12:23:26 +0100

    Introduce statsolly e2e tests
    Signed-off-by: Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>

--- internal/test/integration/red_test_stat_metrics.go
+++ internal/test/integration/red_test_stat_metrics.go
@@ -13,25 +13,21 @@ import (
 	"go.opentelemetry.io/obi/internal/test/integration/components/promtest"
 )
 
-func testAppNetMetricsTCPRtt(t *testing.T, comm, namespace string) {
+func testStatMetricsTCPRtt(t *testing.T, port string) {
 	// Eventually, Prometheus would make this query visible
 	pq := promtest.Client{HostPort: prometheusHostPort}
 	var results []promtest.Result
 	require.EventuallyWithT(t, func(ct *assert.CollectT) {
 		var err error
-		results, err = pq.Query(`obi_net_tcp_rtt_seconds_bucket{` +
-			`host_name="obi",` +
-			`service_namespace="` + namespace + `",` +
-			`service_name="` + comm + `"}`)
+		results, err = pq.Query(`obi_stat_tcp_rtt_seconds_bucket{` +
+			`dst_port="` + port + `"}`)
 		require.NoError(ct, err)
 		enoughPromResults(ct, results)
 		val := totalPromCount(ct, results)
 		assert.LessOrEqual(ct, 1, val)
 
-		results, err = pq.Query(`obi_net_tcp_rtt_seconds_count{` +
-			`host_name="obi",` +
-			`service_namespace="` + namespace + `",` +
-			`service_name="` + comm + `"}`)
+		results, err = pq.Query(`obi_stat_tcp_rtt_seconds_count{` +
+			`dst_port="` + port + `"}`)
 		require.NoError(ct, err)
 		enoughPromResults(ct, results)
 		val = totalPromCount(ct, results)
@@ -39,24 +35,13 @@ func testAppNetMetricsTCPRtt(t *testing.T, comm, namespace string) {
 	}, testTimeout, 100*time.Millisecond)
 }
 
-func testAppNetMetricsTCPRttGo(t *testing.T) {
+func testStatMetricsTCPRttGo(t *testing.T) {
 	for _, testCaseURL := range []string{
 		"http://localhost:8381",
 	} {
 		t.Run(testCaseURL, func(t *testing.T) {
 			waitForTestComponentsTCP(t, testCaseURL)
-			testAppNetMetricsTCPRtt(t, "testserver", "integration-test")
-		})
-	}
-}
-
-func testAppNetMetricsTCPRttPython(t *testing.T) {
-	for _, testCaseURL := range []string{
-		"http://localhost:8381",
-	} {
-		t.Run(testCaseURL, func(t *testing.T) {
-			waitForTestComponentsTCP(t, testCaseURL)
-			testAppNetMetricsTCPRtt(t, "python3.14", "integration-test")
+			testStatMetricsTCPRtt(t, "8080")
 		})
 	}
 }
