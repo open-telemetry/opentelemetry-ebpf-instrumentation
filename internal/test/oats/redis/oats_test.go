75bfe3a7b9d6face28fe5a97d45e34878fbb4790
Tyler Yahn <codingalias@gmail.com>
2026-03-09 12:17:03 -0700

    Downgrade oats to v0.0.3

--- internal/test/oats/redis/oats_test.go
+++ internal/test/oats/redis/oats_test.go
@@ -4,11 +4,8 @@
 package oats
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
-	"github.com/grafana/oats/model"
 	"github.com/grafana/oats/yaml"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -20,35 +17,25 @@ func TestYaml(t *testing.T) {
 }
 
 var _ = Describe("test case", Label("docker", "integration", "slow"), func() {
-	fmt.Println("First test")
-	cases, err := yaml.ReadTestCases([]string{"."}, true)
-	Expect(err).ToNot(HaveOccurred())
-	It("should have at least one test case", func() {
-		Expect(cases).ToNot(BeEmpty(), "expected at least one test case")
-	})
+	cases, base := yaml.ReadTestCases()
+	if base != "" {
+		It("should have at least one test case", func() {
+			Expect(cases).ToNot(BeEmpty(), "expected at least one test case in %s", base)
+		})
+	}
 
-	yaml.VerboseLogging = true
-	settings := model.Settings{
-		Host:          "127.0.0.1",
-		Timeout:       30 * time.Second,
-		AbsentTimeout: 10 * time.Second,
-		LgtmVersion:   "latest",
-		LgtmLogSettings: map[string]bool{
-			"ENABLE_LOGS_ALL":        false,
-			"ENABLE_LOGS_GRAFANA":    false,
-			"ENABLE_LOGS_PROMETHEUS": false,
-			"ENABLE_LOGS_LOKI":       false,
-			"ENABLE_LOGS_TEMPO":      false,
-			"ENABLE_LOGS_PYROSCOPE":  false,
-			"ENABLE_LOGS_OTELCOL":    false,
-		},
-		LogLimit: 1000,
+	configuration, _ := GinkgoConfiguration()
+	if configuration.ParallelTotal > 1 {
+		ports := yaml.NewPortAllocator(len(cases))
+		for _, c := range cases {
+			// Pre-allocate ports before parallel execution to avoid collisions.
+			c.PortConfig = ports.AllocatePorts()
+		}
 	}
 
 	for _, c := range cases {
-		tc := c
 		Describe(c.Name, Ordered, func() {
-			yaml.RunTestCase(&tc, settings)
+			yaml.RunTestCase(c)
 		})
 	}
 })
