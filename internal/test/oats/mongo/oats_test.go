446d8e8a9016116b272fdd9b805ece6766370e3d
Tyler Yahn <codingalias@gmail.com>
2026-03-09 11:18:23 -0700

    Fix OATS tests
    Update code to latest package usage.

--- internal/test/oats/mongo/oats_test.go
+++ internal/test/oats/mongo/oats_test.go
@@ -6,7 +6,9 @@ package oats
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/grafana/oats/model"
 	"github.com/grafana/oats/yaml"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -19,28 +21,34 @@ func TestYaml(t *testing.T) {
 
 var _ = Describe("test case", Label("docker", "integration", "slow"), func() {
 	fmt.Println("First test")
-	cases, base := yaml.ReadTestCases()
-	if base != "" {
-		It("should have at least one test case", func() {
-			Expect(cases).ToNot(BeEmpty(), "expected at least one test case in %s", base)
-		})
-	}
-
-	configuration, _ := GinkgoConfiguration()
-	if configuration.ParallelTotal > 1 {
-		ports := yaml.NewPortAllocator(len(cases))
-		for _, c := range cases {
-			// Ports have to be allocated before we start executing in parallel to avoid taking the same port.
-			// Even though it sounds unlikely, it happens quite often.
-			c.PortConfig = ports.AllocatePorts()
-		}
-	}
+	cases, err := yaml.ReadTestCases([]string{"."}, true)
+	Expect(err).ToNot(HaveOccurred())
+	It("should have at least one test case", func() {
+		Expect(cases).ToNot(BeEmpty(), "expected at least one test case")
+	})
 
 	yaml.VerboseLogging = true
+	settings := model.Settings{
+		Host:          "127.0.0.1",
+		Timeout:       30 * time.Second,
+		AbsentTimeout: 10 * time.Second,
+		LgtmVersion:   "latest",
+		LgtmLogSettings: map[string]bool{
+			"ENABLE_LOGS_ALL":        false,
+			"ENABLE_LOGS_GRAFANA":    false,
+			"ENABLE_LOGS_PROMETHEUS": false,
+			"ENABLE_LOGS_LOKI":       false,
+			"ENABLE_LOGS_TEMPO":      false,
+			"ENABLE_LOGS_PYROSCOPE":  false,
+			"ENABLE_LOGS_OTELCOL":    false,
+		},
+		LogLimit: 1000,
+	}
 
 	for _, c := range cases {
+		tc := c
 		Describe(c.Name, Ordered, func() {
-			yaml.RunTestCase(c)
+			yaml.RunTestCase(&tc, settings)
 		})
 	}
 })
