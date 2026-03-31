cc901639f169c2b2c4ec3a436f5067bdf05f80ad
Tyler Yahn <codingalias@gmail.com>
2026-03-31 11:27:43 -0700

    Up cilium/ebpf ver in placeholder

--- bpf/bpfcore/placeholder.go
+++ bpf/bpfcore/placeholder.go
@@ -5,5 +5,17 @@
 
 package bpfcore // import "go.opentelemetry.io/obi/bpf/bpfcore"
 
-// Store loader version here in order to run the full test suite on upgrades.
-// github.com/cilium/ebpf v0.20.0
+// Below is the version of the cilium/ebpf library that we are using, which is
+// the main dependency of this module.
+//
+// If you are updating the version of cilium/ebpf, update the version here as
+// well. This will trigger a check in the CI to ensure that the version of
+// cilium/ebpf used in this module is consistent with the version used in the
+// main module (go.opentelemetry.io/obi) and that functinality is not broken
+// due to the update.
+//
+// If you are updating the version of cilium/ebpf, also update the version in
+// the go.mod file of this module and the main module (go.opentelemetry.io/obi)
+// to ensure consistency.
+//
+// github.com/cilium/ebpf v0.21.0
