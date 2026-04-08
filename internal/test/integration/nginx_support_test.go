c5d5146af3515e81edaa64d5833e986b424e8cb0
Tyler Yahn <codingalias@gmail.com>
2026-04-07 17:12:24 -0700

    Pin floor test imgs

--- internal/test/integration/nginx_support_test.go
+++ internal/test/integration/nginx_support_test.go
@@ -4,6 +4,6 @@
 package integration
 
 const (
-	nginxServerTracingSupportFloorImage = "nginx:1.27.3"
-	nginxReverseProxySupportFloorImage  = "nginx:1.27.5"
+	nginxServerTracingSupportFloorImage = "nginx:1.27.3@sha256:bc2f6a7c8ddbccf55bdb19659ce3b0a92ca6559e86d42677a5a02ef6bda2fcef"
+	nginxReverseProxySupportFloorImage  = "nginx:1.27.5@sha256:6784fb0834aa7dbbe12e3d7471e69c290df3e6ba810dc38b34ae33d3c1c05f7d"
 )
