b029bc969bc2c36f18dc5025a61a4aabba868b34
Tyler Yahn <codingalias@gmail.com>
2026-04-28 13:24:55 -0700

    prom: export BPF metrics to Prometheus internal reporters

---
+++ pkg/export/imetrics/imetrics_test.go
@@ -0,0 +1,41 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package imetrics
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsBuiltinNoopReporter(t *testing.T) {
+	t.Run("noop reporter value", func(t *testing.T) {
+		assert.True(t, IsBuiltinNoopReporter(NoopReporter{}))
+	})
+
+	t.Run("noop reporter pointer", func(t *testing.T) {
+		assert.True(t, IsBuiltinNoopReporter(&NoopReporter{}))
+	})
+
+	t.Run("prometheus reporter", func(t *testing.T) {
+		reporter := NewPrometheusReporter(&InternalMetricsConfig{}, nil, prometheus.NewRegistry())
+		assert.False(t, IsBuiltinNoopReporter(reporter))
+	})
+
+	t.Run("noop embedder is not builtin noop", func(t *testing.T) {
+		reporter := &noopEmbeddingReporter{}
+		assert.False(t, IsBuiltinNoopReporter(reporter))
+	})
+
+	t.Run("nil reporter", func(t *testing.T) {
+		assert.False(t, IsBuiltinNoopReporter(nil))
+	})
+}
+
+type noopEmbeddingReporter struct {
+	NoopReporter
+}
+
+func (n *noopEmbeddingReporter) BpfProbeLatency(_, _, _ string, _ float64) {}
