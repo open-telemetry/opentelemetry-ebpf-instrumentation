fedece7ae755fd7b2cb900b16b0055e470d60e47
Hectar <84522781+hectar-glitches@users.noreply.github.com>
2026-05-05 13:57:30 +0900

    Update bpf/gotracer/go_str.h
    Co-authored-by: Rafael Roquetto <rafaelroquetto@users.noreply.github.com>

--- bpf/gotracer/go_str.h
+++ bpf/gotracer/go_str.h
@@ -49,7 +49,7 @@ read_go_str(char *name, void *base_ptr, u8 offset, void *field, u64 max_size) {
     }
 
     u64 len = 0;
-    if (bpf_probe_read(&len, sizeof(len), (void *)(base_ptr + (offset + GO_STRING_LEN_OFFSET))) !=
+    if (bpf_probe_read(&len, sizeof(len), (void *)(base_ptr + (offset + k_go_string_len_offset))) !=
         0) {
         bpf_dbg_printk("can't read len for %s", name);
         return 0;
