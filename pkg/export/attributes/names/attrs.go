8ef64c7454c5596e42cdfed501b7422500f14611
zhb192729 <zhb192729@alibaba-inc.com>
2026-05-08 22:42:52 +0800

    fix comment

--- pkg/export/attributes/names/attrs.go
+++ pkg/export/attributes/names/attrs.go
@@ -34,7 +34,7 @@ const (
 	HTTPURLScheme          = Name(semconv.URLSchemeKey)
 	HTTPUrlPath            = Name(semconv.URLPathKey)
 	HTTPUrlFull            = Name(semconv.URLFullKey)
-	HTTPUrlQuery           = Name("url.query")
+	HTTPUrlQuery           = Name(semconv.URLQueryKey)
 	ClientAddr             = Name(semconv.ClientAddressKey)
 	ServerAddr             = Name(semconv.ServerAddressKey)
 	ServerPort             = Name(semconv.ServerPortKey)
