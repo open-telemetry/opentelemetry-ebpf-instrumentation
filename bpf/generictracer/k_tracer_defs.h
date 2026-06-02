ef31c6fac679585ead84b3555aef247e63c9a6df
Tyler Yahn <codingalias@gmail.com>
2026-06-02 07:52:04 -0700

    fix(bpf): couple sniff buffer clamp to buffer size

--- bpf/generictracer/k_tracer_defs.h
+++ bpf/generictracer/k_tracer_defs.h
@@ -62,7 +62,7 @@ static __always_inline call_protocol_args_t *make_protocol_args(const pid_connec
     __builtin_memset(args->small_buf, 0, sizeof(args->small_buf));
     if (bytes_len > 0) {
         u32 small_buf_len = (u32)bytes_len;
-        bpf_clamp_umax(small_buf_len, MIN_HTTP2_SIZE);
+        bpf_clamp_umax(small_buf_len, sizeof(args->small_buf));
         bpf_probe_read(args->small_buf, small_buf_len, (void *)args->u_buf);
     }
 
