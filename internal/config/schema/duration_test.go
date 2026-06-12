0f8b13b67c79c17913d282526e65899f6717ebd9
Tyler Yahn <codingalias@gmail.com>
2026-06-12 08:26:08 -0700

    Add schema decoder test coverage

--- internal/config/schema/duration_test.go
+++ internal/config/schema/duration_test.go
@@ -27,6 +27,49 @@ func TestDurationYAML(t *testing.T) {
 	require.Equal(t, "ttl: 5m0s\n", string(data))
 }
 
+func TestDurationYAMLNullAndErrors(t *testing.T) {
+	t.Parallel()
+
+	t.Run("null", func(t *testing.T) {
+		t.Parallel()
+
+		ttl := Duration(time.Second)
+
+		require.NoError(t, ttl.UnmarshalYAML(&yaml.Node{Tag: "!!null"}))
+		require.Equal(t, Duration(0), ttl)
+	})
+
+	tests := []struct {
+		name string
+		yaml string
+		err  string
+	}{
+		{
+			name: "non scalar",
+			yaml: "ttl: []\n",
+			err:  "duration must be a scalar",
+		},
+		{
+			name: "invalid duration",
+			yaml: "ttl: nope\n",
+			err:  "parse duration",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			var doc struct {
+				TTL Duration `yaml:"ttl"`
+			}
+
+			err := yaml.Unmarshal([]byte(test.yaml), &doc)
+			require.Error(t, err)
+			require.Contains(t, err.Error(), test.err)
+		})
+	}
+}
+
 func TestMillisecondsYAML(t *testing.T) {
 	t.Parallel()
 
@@ -41,4 +84,47 @@ func TestMillisecondsYAML(t *testing.T) {
 	data, err := yaml.Marshal(doc)
 	require.NoError(t, err)
 	require.Equal(t, "interval: 1000\n", string(data))
+}
+
+func TestMillisecondsYAMLNullAndErrors(t *testing.T) {
+	t.Parallel()
+
+	t.Run("null", func(t *testing.T) {
+		t.Parallel()
+
+		interval := Milliseconds(time.Second)
+
+		require.NoError(t, interval.UnmarshalYAML(&yaml.Node{Tag: "!!null"}))
+		require.Equal(t, Milliseconds(0), interval)
+	})
+
+	tests := []struct {
+		name string
+		yaml string
+		err  string
+	}{
+		{
+			name: "non scalar",
+			yaml: "interval: []\n",
+			err:  "milliseconds must be a scalar",
+		},
+		{
+			name: "invalid milliseconds",
+			yaml: "interval: nope\n",
+			err:  "parse milliseconds",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			var doc struct {
+				Interval Milliseconds `yaml:"interval"`
+			}
+
+			err := yaml.Unmarshal([]byte(test.yaml), &doc)
+			require.Error(t, err)
+			require.Contains(t, err.Error(), test.err)
+		})
+	}
 }
---
+++ internal/config/schema/otel_test.go
@@ -0,0 +1,40 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package schema
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/obi/pkg/export/otel/otelcfg"
+	"gopkg.in/yaml.v3"
+)
+
+func TestOTLPGRPCExporterDefaultHistogramAggregationYAML(t *testing.T) {
+	t.Parallel()
+
+	var exporter OTLPGRPCExporter
+	err := yaml.Unmarshal([]byte(`
+endpoint: http://localhost:4317
+default_histogram_aggregation: base2_exponential_bucket_histogram
+tls:
+  insecure: true
+`), &exporter)
+	require.NoError(t, err)
+	require.Equal(t, "http://localhost:4317", exporter.Endpoint)
+	require.Equal(t, otelcfg.HistogramAggregationExponential, exporter.DefaultHistogramAggregation)
+	require.True(t, exporter.TLS.Insecure)
+}
+
+func TestOTLPGRPCExporterRejectsInvalidNestedYAML(t *testing.T) {
+	t.Parallel()
+
+	var exporter OTLPGRPCExporter
+	err := yaml.Unmarshal([]byte(`
+retry:
+  initial_interval: []
+`), &exporter)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "duration must be a scalar")
+}
