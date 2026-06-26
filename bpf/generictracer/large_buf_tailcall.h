c5445d150677d50bcb73fadec2dde3d82a57d0e7
zhb192729 <zhb192729@alibaba-inc.com>
2026-06-26 10:01:45 +0800

    merge main

--- bpf/generictracer/large_buf_tailcall.h
+++ bpf/generictracer/large_buf_tailcall.h
@@ -18,7 +18,7 @@ typedef struct large_buf_emit_state {
     u8 batch_iter;
     u8 packet_type;
     u8 direction;
-    u8 action;
+    enum large_buf_action action;
 } large_buf_emit_state_t;
 
 SCRATCH_MEM_TYPED(large_buf_emit_state, large_buf_emit_state_t)
