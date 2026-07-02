979380a4f20613684144d3dfb71b2aaa2ce86d96
NimrodAvni78 <73278770+NimrodAvni78@users.noreply.github.com>
2026-07-02 17:52:17 +0300

    Update pkg/export/attributes/names/attrs.go
    Co-authored-by: Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>

--- pkg/export/attributes/names/attrs.go
+++ pkg/export/attributes/names/attrs.go
@@ -222,7 +222,7 @@ const (
 	DBResponseStatusCode = Name(semconv.DBResponseStatusCodeKey)
 	DBNamespace          = Name(semconv.DBNamespaceKey)
 	DBResponseError      = Name("db.response.error")
-	DBOperationBatchSize = Name("db.operation.batch.size")
+	DBOperationBatchSize = Name(semconv.DBOperationBatchSizeKey)
 
 	// Messaging
 	MessagingPartition   = Name(semconv.MessagingDestinationPartitionIDKey)
