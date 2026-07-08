fed52b0603dd6700e576cd62106196c5812b18fe
chengshuyi <chengshuyi@linux.alibaba.com>
2026-07-08 21:18:11 +0800

    style(ebpf): remove unnecessary nil check in connection summary role getter
    When Stat.Type is StatTypeTCPConnectionSummary, the TCPConnectionSummary
    field is always non-nil (set by the BPF event handler). Remove the
    redundant nil check for consistency with TCPRtt and TCPFailedConnection
    cases which also access their fields directly.
    
    Signed-off-by: chengshuyi <chengshuyi@linux.alibaba.com>

--- pkg/internal/statsolly/ebpf/stat_getters.go
+++ pkg/internal/statsolly/ebpf/stat_getters.go
@@ -59,9 +59,7 @@ func StatGetters(name attr.Name) (attributes.Getter[*Stat, attribute.KeyValue], 
 			case StatTypeTCPRtt:
 				role = s.TCPRtt.Role
 			case StatTypeTCPConnectionSummary:
-				if s.TCPConnectionSummary != nil {
-					role = s.TCPConnectionSummary.Role
-				}
+				role = s.TCPConnectionSummary.Role
 			}
 			return attribute.String(string(attr.NetworkTCPHandshakeRole), networkTCPHandshakeRoleStr(role))
 		}
