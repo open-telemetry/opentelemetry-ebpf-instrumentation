f1b51914c23240b73faa463a2b44bda4d5f2562d
Marc Tuduri <marctc@protonmail.com>
2026-08-04 16:25:30 +0200

    PR feedback

--- bpf/generictracer/protocol_http.h
+++ bpf/generictracer/protocol_http.h
@@ -27,6 +27,7 @@
 
 #include <generictracer/k_tracer_tailcall.h>
 #include <generictracer/large_buf_tailcall.h>
+#include <generictracer/http_server_trace.h>
 #include <generictracer/protocol_common.h>
 
 #include <logger/bpf_dbg.h>
@@ -122,37 +123,13 @@ static __always_inline void cleanup_http_server_response_data(pid_connection_inf
     }
 }
 
-// Returns the thread trace only if it still belongs to this request.
-static __always_inline tp_info_pid_t *http_server_thread_trace(http_info_t *info,
-                                                               trace_key_t *t_key) {
-    t_key->extra_id = info->extra_id;
-    t_key->p_key.ns = info->pid.ns;
-    t_key->p_key.tid = info->task_tid;
-    t_key->p_key.pid = info->pid.user_pid;
-
-    tp_info_pid_t *existing = bpf_map_lookup_elem(&server_traces, t_key);
-    if (!existing) {
-        return NULL;
-    }
-
-    if (bpf_memcmp(existing->tp.trace_id, info->tp.trace_id, TRACE_ID_SIZE_BYTES) != 0 ||
-        bpf_memcmp(existing->tp.span_id, info->tp.span_id, SPAN_ID_SIZE_BYTES) != 0) {
-        bpf_dbg_printk("server thread trace for tid=%d replaced by a newer request",
-                       t_key->p_key.tid);
-        return NULL;
-    }
-
-    return existing;
-}
-
 static __always_inline void cleanup_http_server_thread_trace(http_info_t *info) {
-    trace_key_t t_key = {0};
-    if (!http_server_thread_trace(info, &t_key)) {
-        return;
+    trace_key_t current_key = {0};
+    if (info->delayed) {
+        trace_key_from_pid_tid(&current_key);
     }
 
-    int res = bpf_map_delete_elem(&server_traces, &t_key);
-    bpf_dbg_printk("Deleting server thread trace for tid=%d, res=%d", t_key.p_key.tid, res);
+    cleanup_http_server_thread_trace_for_key(info, &current_key);
 }
 
 static __always_inline void cleanup_incomplete_http_server_thread_trace(http_info_t *info) {
