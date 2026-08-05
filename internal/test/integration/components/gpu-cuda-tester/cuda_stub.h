890323b6bd8ba5926148eb4b41957f8743df6cc5
NimrodAvni78 <nimrod@coralogix.com>
2026-08-05 11:07:22 +0300

    format + fix exe matcher

--- internal/test/integration/components/gpu-cuda-tester/cuda_stub.h
+++ internal/test/integration/components/gpu-cuda-tester/cuda_stub.h
@@ -39,7 +39,8 @@ enum cudaMemcpyKind {
 };
 
 cudaError_t cudaLaunchKernel(const void *func, dim3 gridDim, dim3 blockDim,
-                             void **args, size_t sharedMem, cudaStream_t stream);
+                             void **args, size_t sharedMem,
+                             cudaStream_t stream);
 cudaError_t cudaMalloc(void **devPtr, size_t size);
 cudaError_t cudaMemcpy(void *dst, const void *src, size_t count,
                        enum cudaMemcpyKind kind);
