b0819a8728f0b1c2c568d84b4560586dda12ef71
Nikola Grcevski <grcevski@gmail.com>
2026-08-20 19:39:30 -0400

    fix one more test

--- internal/test/integration/red_test_mqtt.go
+++ internal/test/integration/red_test_mqtt.go
@@ -113,7 +113,7 @@ func testREDMetricsPythonMQTTSubscribe(t *testing.T) {
 		{
 			Route:   "http://localhost:8381",
 			Subpath: "mqtt/subscribe",
-			Comm:    "python3.14",
+			Comm:    "pythonmqtt",
 			Spans: []TestCaseSpan{
 				{
 					Name: "process test/topic",
