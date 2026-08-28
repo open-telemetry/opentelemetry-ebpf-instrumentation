722fe82aae32ce6e54f7a25fe39748a547278b96
Nikola Grcevski <6207777+grcevski@users.noreply.github.com>
2026-08-28 09:46:16 -0400

    dotnet: Name .NET apps as the OpenTelemetry .NET SDK (#3118)

--- pkg/appolly/discover/attacher.go
+++ pkg/appolly/discover/attacher.go
@@ -20,6 +20,7 @@ import (
 	"go.opentelemetry.io/obi/pkg/ebpf"
 	ebpfcommon "go.opentelemetry.io/obi/pkg/ebpf/common"
 	"go.opentelemetry.io/obi/pkg/export/imetrics"
+	"go.opentelemetry.io/obi/pkg/internal/dotnettools"
 	"go.opentelemetry.io/obi/pkg/internal/helpers/maps"
 	javaagent "go.opentelemetry.io/obi/pkg/internal/java"
 	"go.opentelemetry.io/obi/pkg/internal/jvmtools"
@@ -196,6 +197,11 @@ func (ta *traceAttacher) resolveServiceMetadata(ie *ebpf.Instrumentable) {
 		err := pythontools.ResolveServiceMetadata(ie.FileInfo)
 		if err != nil {
 			ta.log.Debug("unable to resolve Python service metadata", "pid", ie.FileInfo.Pid(), "error", err)
+		}
+	case svc.InstrumentableDotnet:
+		err := dotnettools.ResolveServiceMetadata(ie.FileInfo)
+		if err != nil {
+			ta.log.Debug("unable to resolve .NET service metadata", "pid", ie.FileInfo.Pid(), "error", err)
 		}
 	}
 }
