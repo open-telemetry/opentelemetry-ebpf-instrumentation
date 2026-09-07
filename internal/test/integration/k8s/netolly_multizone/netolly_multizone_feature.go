a966029eb958f7156045278408a4dd7fd86da261
NimrodAvni78 <nimrod@coralogix.com>
2026-09-07 15:30:53 +0300

    test: add netolly k8s assertions

--- internal/test/integration/k8s/netolly_multizone/netolly_multizone_feature.go
+++ internal/test/integration/k8s/netolly_multizone/netolly_multizone_feature.go
@@ -37,12 +37,12 @@ func testFlowsDecoratedWithZone(ctx context.Context, t *testing.T, _ *envconf.Co
 			`k8s_src_name="httppinger",k8s_dst_name=~"testserver.*",` +
 			`k8s_src_type="Pod",k8s_dst_type="Pod"` +
 			`}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 
 		// check that the metrics are properly decorated
 		// should have 2 exact metrics, measured from OBI instances in both nodes
-		require.GreaterOrEqual(ct, len(results), 2)
+		assert.GreaterOrEqual(ct, len(results), 2)
 		for _, res := range results {
 			assert.Equal(ct, "client-zone", res.Metric["src_zone"])
 			assert.Equal(ct, "server-zone", res.Metric["dst_zone"])
@@ -54,12 +54,12 @@ func testFlowsDecoratedWithZone(ctx context.Context, t *testing.T, _ *envconf.Co
 			`k8s_dst_name="httppinger",k8s_src_name=~"testserver.*",` +
 			`k8s_src_type="Pod",k8s_dst_type="Pod"` +
 			`}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 
 		// check that the metrics are properly decorated
 		// should have 2 exact metrics, measured from OBI instances in both nodes
-		require.GreaterOrEqual(ct, len(results), 2)
+		assert.GreaterOrEqual(ct, len(results), 2)
 		for _, res := range results {
 			assert.Equal(ct, "server-zone", res.Metric["src_zone"])
 			assert.Equal(ct, "client-zone", res.Metric["dst_zone"])
@@ -72,48 +72,48 @@ func testFlowsDecoratedWithZone(ctx context.Context, t *testing.T, _ *envconf.Co
 			`src_zone="server-zone",dst_zone="control-plane-zone",` +
 			`k8s_src_type="Node",k8s_dst_type="Node"` +
 			`}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 
 		// check that the metrics are properly decorated
 		// should have 2 exact metrics, measured from OBI instances in both nodes
-		require.GreaterOrEqual(ct, len(results), 2)
+		assert.GreaterOrEqual(ct, len(results), 2)
 	}, testTimeout, 100*time.Millisecond)
 	require.EventuallyWithT(t, func(ct *assert.CollectT) {
 		results, err := pq.Query(`obi_network_flow_bytes_total{` +
 			`dst_zone="server-zone",src_zone="control-plane-zone",` +
 			`k8s_src_type="Node",k8s_dst_type="Node"` +
 			`}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 
 		// check that the metrics are properly decorated
 		// should have 2 exact metrics, measured from OBI instances in both nodes
-		require.GreaterOrEqual(ct, len(results), 2)
+		assert.GreaterOrEqual(ct, len(results), 2)
 	}, testTimeout, 100*time.Millisecond)
 	require.EventuallyWithT(t, func(ct *assert.CollectT) {
 		results, err := pq.Query(`obi_network_flow_bytes_total{` +
 			`src_zone="client-zone",dst_zone="control-plane-zone",` +
 			`k8s_src_type="Node",k8s_dst_type="Node"` +
 			`}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 
 		// check that the metrics are properly decorated
 		// should have 2 exact metrics, measured from OBI instances in both nodes
-		require.GreaterOrEqual(ct, len(results), 2)
+		assert.GreaterOrEqual(ct, len(results), 2)
 	}, testTimeout, 100*time.Millisecond)
 	require.EventuallyWithT(t, func(ct *assert.CollectT) {
 		results, err := pq.Query(`obi_network_flow_bytes_total{` +
 			`dst_zone="client-zone",src_zone="control-plane-zone",` +
 			`k8s_src_type="Node",k8s_dst_type="Node"` +
 			`}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 
 		// check that the metrics are properly decorated
 		// should have 2 exact metrics, measured from OBI instances in both nodes
-		require.GreaterOrEqual(ct, len(results), 2)
+		assert.GreaterOrEqual(ct, len(results), 2)
 	}, testTimeout, 100*time.Millisecond)
 	return ctx
 }
@@ -125,17 +125,18 @@ func testInterZoneMetric(ctx context.Context, t *testing.T, _ *envconf.Config) c
 	require.EventuallyWithT(t, func(ct *assert.CollectT) {
 		results, err := pq.Query(`obi_network_inter_zone_bytes_total{` +
 			`src_zone="client-zone", dst_zone="server-zone"}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
 	}, testTimeout, 100*time.Millisecond)
 	require.EventuallyWithT(t, func(ct *assert.CollectT) {
 		results, err := pq.Query(`obi_network_inter_zone_bytes_total{` +
 			`dst_zone="client-zone", src_zone="server-zone"}`)
-		require.NoError(ct, err)
-		require.NotEmpty(ct, results)
-		// AND the reported attributes are different from the flow bytes attributes
-		require.NotContains(ct, results, "k8s_src_type")
-		require.NotContains(ct, results, "iface_direction")
+		assert.NoError(ct, err)
+		assert.NotEmpty(ct, results)
+		for _, res := range results {
+			assert.Contains(ct, res.Metric, "k8s_src_type")
+			assert.Contains(ct, res.Metric, "iface_direction")
+		}
 	}, testTimeout, 100*time.Millisecond)
 
 	// BUT same-zone bytes are not reported in this metric
