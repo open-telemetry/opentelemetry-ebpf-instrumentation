86c855f2497f2f273a8ac0178153b8fdd53e5dcc
Nikola Grcevski <grcevski@gmail.com>
2026-09-13 14:21:44 -0400

    add integration tests

--- internal/test/integration/php_fm_test.go
+++ internal/test/integration/php_fm_test.go
@@ -122,6 +122,7 @@ func TestPHPFM(t *testing.T) {
 
 	t.Run("PHP-FM RED metrics", testREDMetricsPHPFPM)
 	t.Run("PHP Symfony", testPHPSymfony)
+	t.Run("PHP harvested routes", testPHPHarvestedRoutes)
 
 	runWeaverValidation(t)
 
