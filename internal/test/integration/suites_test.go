b081b1333ab219d3f5da0c776f6932c57ff9a1e2
Mario Macias <mario.macias@grafana.com>
2026-09-14 18:46:09 +0200

    Ruby on Rails routes harvester (#3374)
    * Ruby on Rails routes harvester
    
    * Addressed code comments
    
    * Fix broken integration tests after merge

--- internal/test/integration/suites_test.go
+++ internal/test/integration/suites_test.go
@@ -455,6 +455,7 @@ func TestSuite_Rails(t *testing.T) {
 	compose.Env = append(compose.Env, `OTEL_EBPF_OPEN_PORT=3040,443`, `OTEL_EBPF_EXECUTABLE_PATH=`, `TEST_SERVICE_PORTS=3041:3040`, `TESTSERVER_IMAGE=`+obiTestImgRails)
 	require.NoError(t, compose.Up())
 	t.Run("Rails RED metrics", func(t *testing.T) { testREDMetricsRailsHTTP(t, "testapi") })
+	t.Run("Rails harvested routes", func(t *testing.T) { testRailsHarvestedRoutes(t, "testapi") })
 	t.Run("Rails NGINX traces", testHTTPTracesNestedNginx)
 	runWeaverValidation(t)
 	require.NoError(t, compose.Close())
@@ -511,7 +512,9 @@ func TestSuite_RailsTLS(t *testing.T) {
 	compose, err := docker.ComposeSuite("docker-compose-ruby.yml", path.Join(pathOutput, "test-suite-ruby-tls.log"))
 	require.NoError(t, err)
 
-	compose.Env = append(compose.Env, `OTEL_EBPF_OPEN_PORT=3043`, `OTEL_EBPF_EXECUTABLE_PATH=`, `TESTSERVER_IMAGE=`+obiTestImgRailsSSL, `TEST_SERVICE_PORTS=3044:3043`)
+	// TODO: remove TESTSERVER_DOCKERFILE_SUFFIX once docker-compose-ruby.yml uses again an "image" clause
+	compose.Env = append(compose.Env, `OTEL_EBPF_OPEN_PORT=3043`, `OTEL_EBPF_EXECUTABLE_PATH=`, `TESTSERVER_IMAGE=`+obiTestImgRailsSSL, `TEST_SERVICE_PORTS=3044:3043`,
+		`TESTSERVER_DOCKERFILE_SUFFIX=_tls`)
 	require.NoError(t, compose.Up())
 	t.Run("Rails SSL RED metrics", func(t *testing.T) { testREDMetricsRailsHTTPS(t, "testapi") })
 	runWeaverValidation(t)
