91a816aee3e20166ed68a78b36e2b132a611eb03
Nikola Grcevski <6207777+grcevski@users.noreply.github.com>
2026-09-15 16:07:23 -0400

    php: Name php apps like OpenTelemetry PHP SDK (#3398)

--- internal/test/integration/php_fm_test.go
+++ internal/test/integration/php_fm_test.go
@@ -121,6 +121,7 @@ func TestPHPFM(t *testing.T) {
 	require.NoError(t, compose.Up())
 
 	t.Run("PHP-FM RED metrics", testREDMetricsPHPFPM)
+	t.Run("PHP Symfony", testPHPSymfony)
 
 	runWeaverValidation(t)
 
